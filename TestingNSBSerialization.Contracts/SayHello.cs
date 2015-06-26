3ad14567c26bdbe167c1df8a65a7fed0d87bb06e
Fawad Halim <fawad@fawad.net>
2015-06-26 08:24:39 -0600

    Initial code demoing JSON support

---
+++ TestingNSBSerialization.Contracts/SayHello.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingNSBSerialization.Contracts
+{
+    using NServiceBus;
+
+    public class SayHello:ICommand
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ TestingNSBSerialization.Printer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingNSBSerialization.Printer
+{
+    using Contracts;
+    using NServiceBus;
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.UseTransport<RabbitMQTransport>().DisableCallbackReceiver()
+                .ConnectionString("host=localhost");
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.ScaleOut().UseSingleBrokerQueue();
+            using (var bus = Bus.Create(busConfiguration)) {
+                bus.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class HelloWriter : IHandleMessages<SayHello>
+    {
+        public void Handle(SayHello message)
+        {
+            Console.WriteLine("Hello, {0}", message.Message);
+        }
+    }
+}
