961a7d185d18fb184187e00d0e342894a1bd7162
Fawad Halim <fawad@fawad.net>
2015-06-29 07:14:25 -0600

    Added missing files.

---
+++ TestingNSBSerialization.Printer/HelloWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace TestingNSBSerialization.Printer
+{
+    using System;
+    using Contracts;
+    using NServiceBus;
+
+    public class HelloWriter : IHandleMessages<SayHello>
+    {
+        public void Handle(SayHello message)
+        {
+            Console.WriteLine("Hello, {0}", message.Message);
+        }
+    }
+}
